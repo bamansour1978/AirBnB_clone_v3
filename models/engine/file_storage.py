2713dca4261314d0f3d6269273d77255756845e6
bamansour1978 <karatman@gmail.com>
2023-10-30 22:26:30 +0100

    Upadate

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Handles I/O, writing and reading, of JSON for storage of all class instances
+B & L Handles I/O, writing and reading, of JSON for storage of all class instances
 """
 import json
 from models import base_model, amenity, city, place, review, state, user
