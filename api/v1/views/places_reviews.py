d21b561c268a2a1824e0248ef0efda205effb203
bamansour1978 <karatman@gmail.com>
2023-10-30 22:44:19 +0100

    Update

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Flask route that returns json status response
+B & L Flask route that returns json status response
 """
 from api.v1.views import app_views
 from flask import abort, jsonify, request
